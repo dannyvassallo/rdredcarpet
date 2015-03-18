033b8d35ef703a8851f4e1e46d75351a725e7c53
dannyvassallo <danielvassallo87@gmail.com>
2015-03-18 14:04:38 -0400

    removed js for overlay

--- source/javascripts/_overlay.js
+++
@@ -1,8 +0,0 @@
-function overlayMargin(){
-  var headH = $('img.fullw.header').height();
-  $('.overlay').css({'margin-top':headH});
-}
-
-$(window).resize(function(){
-  overlayMargin();
-});
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,4 @@
 //= require jquery
-//= require_overlay
 //= require_alertify
 //= require_validation
 //= require_tree .
